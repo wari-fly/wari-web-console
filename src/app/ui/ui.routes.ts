e764c2f000b173a5b9464dbd4d7e416d8641fa2c
lxpary <lx7pary@gmail.com>
2018-11-16 08:58:20 -0500

    change languaje

--- src/app/ui/ui.routes.ts
+++ src/app/ui/ui.routes.ts
@@ -8,13 +8,13 @@ export const UIRoutes: Routes = [
         path: '',
         component: UiComponent,
         canActivate: [AuthGuard],
-        data: { breadcrumbs: true, text: 'Home' },
+        data: { breadcrumbs: true, text: 'Inicio' },
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', loadChildren: '../ui/dashboard/dashboard.module#DashboardModule', data: { breadcrumbs: true, text: 'Home' } },
-            { path: 'maintenance', loadChildren: '../ui/maintenance/maintenance.module#MaintenanceModule', data: { breadcrumbs: true, text: 'Maintenance' } },
-            { path: 'tracking', loadChildren: '../ui/tracking/tracking.module#TrackingModule', data: { breadcrumbs: true, text: 'Tracking' } },
-            { path: 'user', loadChildren: '../ui/user/user.module#UserModule', data: { breadcrumbs: true, text: 'User' } }
+            { path: 'home', loadChildren: '../ui/dashboard/dashboard.module#DashboardModule', data: { breadcrumbs: true, text: 'Inicio' } },
+            { path: 'maintenance', loadChildren: '../ui/maintenance/maintenance.module#MaintenanceModule', data: { breadcrumbs: true, text: 'Registro de Datos' } },
+            { path: 'tracking', loadChildren: '../ui/tracking/tracking.module#TrackingModule', data: { breadcrumbs: true, text: 'Localizaciones' } },
+            { path: 'user', loadChildren: '../ui/user/user.module#UserModule', data: { breadcrumbs: true, text: 'Usuarios' } }
         ]
     },
     {
